5a671fd9028a6eeec5fe05edcf86e15906a11442
Shane <me@shaneis.me>
2013-04-11 14:24:20 -0700

    BS3-b3 build + Google Fonts

---
+++ index.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang="en" xmlns="http://www.w3.org/1999/xhtml">
+<head>
+    <meta http-equiv="X-UA-Compatible" content="IE=Edge,chrome=1" />
+    <meta charset="utf-8">
+    <title>Duff is us</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="Shane is me">
+    <meta name="author" content="Shane">
+    <meta name="keywords" content="Duff is us" />  
+    <meta name="application-name" content="Duff is us" />
+    <meta name="msapplication-tooltip" content="DI.U" />
+    <meta name="msapplication-window" content="width=1024;height=768" />
+    <meta name="msapplication-starturl" content="http://duffis.us" />
+    <link href='http://fonts.googleapis.com/css?family=PT+Sans' rel='stylesheet' type='text/css'>
+    <link href="assets/css/bootstrap.3.0.0-b3.css" rel="stylesheet" media="screen"> 
+    <link href="assets/css/main.css" rel="stylesheet" media="screen">    
+    <!--[if lt IE 9]>
+      <script src="assets/js/html5shiv.js"></script>
+      <script src="assets/js/respond/respond.js"></script>
+    <![endif]-->  
+</head>
+
+<body>
+
+<div class="container">
+
+  <div class="row">
+  
+    <div class="col-span-12">
+    
+      <h1>Duff is us</h1>
+      
+      <p>Coming soon.</p>
+    
+    </div><!-- .col-span-12 -->
+  
+  </div><!-- .row -->
+
+
+</div><!-- .container -->
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
+<script>window.jQuery || document.write('<script src="assets/js/jquery.1.9.1.min.js">\x3C/script>')</script>
+<script src="assets/js/bootstrap.3.0.0.min.js"></script>
+<script src="assets/js/main.js"></script>
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-40064172-1', 'duffis.us');
+  ga('send', 'pageview');
+
+</script>
+</body>
+</html>
